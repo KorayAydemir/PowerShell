889347980966f3ac5d6cb3df898bdc0cb9db6e93
Koray Aydemir <korayaydemir276@gmail.com>
2024-09-14 08:05:36 +0300

    fix fd depth and add prompt for 'open new tab in current dir' to work

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,19 @@ Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function ForwardWord
 
 function fd {
     $code_dir = "$HOME\dev\"
-    $path = gci -path $code_dir -name -recurse -directory -depth 1 | fzf
+    $path = gci -path $code_dir -name -recurse -directory -depth 2 | fzf
     if ($path) {
         Set-Location $code_dir$path
     }
 }
 
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+  $out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
+}
